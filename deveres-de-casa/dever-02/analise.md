d38755cdca7c0a9f8a784ef244fbdf7a220c77c0
DavidLBO <davidlopes009@gmail.com>
2026-03-12 20:30:33 -0300

    Versão final do código e arquivo analise.md com a análise de complexidade e os resultados do tempo de execução.

---
+++ deveres-de-casa/dever-02/analise.md
@@ -0,0 +1,16 @@
+## Análise de Desempenho do Algoritmo Fatorial Recursivo
+
+Resultados obtidos do tempo de execução:
+
+n = 10 -> 0.00000191 seconds
+n = 100 -> 0.00001693 seconds
+n = 500 -> 0.00013757 seconds
+n = 1000 -> recursion limit reached
+
+## Análise da Complexidade
+
+O algoritmo de fatorial recursivo apresentado opera em O(n). Cada chamada realiza apenas uma operação de forma constante, a multiplicação, e após isso a chamada da função.
+
+Isso resulta em um crescimento linear no tempo de execução, conforme o valor de n aumentar.
+
+Obs: Dentro do python há um limite na profundidade de recursão, o tratamento do erro RecursionError está presente no código para lidar adequadamente com esta situação.
--- deveres-de-casa/dever-02/fatorial.py
+++ deveres-de-casa/dever-02/fatorial.py
@@ -1,3 +1,7 @@
+import time
+
+TEST_VALUES = [10, 100, 500, 1000]
+
 def fatorial(n):
     """
     Calculates the factorial of a number using recursion.
@@ -8,18 +12,40 @@ def fatorial(n):
     Returns:
         int: Factorial of n.
     """
-    if n == 1:
+    if n <= 1:
         return 1
 
     return n * fatorial(n - 1)
 
+def time_measurement(n):
+    """
+    Measures execution time of the factorial function.
+
+    Args:
+        n (int): Input value for factorial.
+
+    Returns:
+        float: Execution time in seconds.
+    """
+    start_time = time.time()
+    fatorial(n)
+    end_time = time.time()
+
+    return end_time - start_time
 
 def main():
     """
     Entry point of the program.
+    Runs execution time tests for different values of n.
     """
-    print(fatorial(6))
+    for n in TEST_VALUES:
+        try:
+            execution_time = time_measurement(n)
+            print(f"n = {n} -> {execution_time:.8f} seconds")
+        except RecursionError:
+            print(f"n = {n} -> recursion limit reached")
 
 
 if __name__ == "__main__":
     main()
+
