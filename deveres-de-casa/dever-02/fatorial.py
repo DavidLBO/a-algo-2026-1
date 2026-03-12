a637ca2167146b956ba0c1fa0a84407ece86d6f2
DavidLBO <davidlopes009@gmail.com>
2026-03-12 20:06:28 -0300

    Corrigindo a função e adicionando docstring

--- deveres-de-casa/dever-02/fatorial.py
+++ deveres-de-casa/dever-02/fatorial.py
@@ -1,13 +1,25 @@
 def fatorial(n):
-    resultado = n
-    if(n == 1):
-        return resultado
-    else:
-        resultado += fatorial(n - 1)
-    return resultado 
+    """
+    Calculates the factorial of a number using recursion.
+
+    Args:
+        n (int): Positive integer.
+
+    Returns:
+        int: Factorial of n.
+    """
+    if n == 1:
+        return 1
+
+    return n * fatorial(n - 1)
+
 
 def main():
+    """
+    Entry point of the program.
+    """
     print(fatorial(6))
+
 
 if __name__ == "__main__":
     main()
